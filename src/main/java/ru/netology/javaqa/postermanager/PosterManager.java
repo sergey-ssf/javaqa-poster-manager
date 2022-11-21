f376e2c886d65ba3969b7b1cabe9e4c4bfe3fc2d
Sergey Sakhanskiy <ssf808867@gmail.com>
2022-11-21 19:38:34 +0300

    Task done

---
+++ src/main/java/ru/netology/javaqa/postermanager/PosterManager.java
@@ -0,0 +1,46 @@
+package ru.netology.javaqa.postermanager;
+
+public class PosterManager {
+
+    private String[] posters = new String[0];
+    private int limit;
+
+    public PosterManager() {
+        this.limit = 10;
+    }
+
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addPoster(String newPoster) {
+        String[] tmp = new String[posters.length + 1];
+        for (int i = 0; i < posters.length; i++) {
+            tmp[i] = posters[i];
+        }
+        tmp[tmp.length - 1] = newPoster;
+        posters = tmp;
+    }
+
+
+    //  Далее эквивалент findAll
+    public String[] giveAllPosters() {
+        return posters;
+    }
+
+
+    // Далее эквивалент findLast с другим названием
+    public String[] giveLastPosters(int limit) {
+        int resultLength;
+        if (posters.length < limit) {
+            resultLength = posters.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < resultLength; i++) {
+            tmp[i] = posters[posters.length - 1 - i];
+        }
+        return tmp;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/postermanager/PosterManagerTest.java
@@ -0,0 +1,61 @@
+package ru.netology.javaqa.postermanager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+
+    @Test
+    public void giveAllPostersTest() {
+        PosterManager movies = new PosterManager();
+        movies.addPoster("Movie 1");
+        movies.addPoster("Movie 2");
+        movies.addPoster("Movie 3");
+
+        String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
+        String[] actual = movies.giveAllPosters();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void giveLast6PostersTest() {
+        PosterManager movies = new PosterManager();
+        movies.addPoster("Hero 1");
+        movies.addPoster("Hero 2");
+        movies.addPoster("Hero 3");
+        movies.addPoster("Hero 4");
+        movies.addPoster("Hero 5");
+        movies.addPoster("Hero 6");
+        movies.addPoster("Hero 7");
+        movies.addPoster("Hero 8");
+        movies.addPoster("Hero 9");
+        movies.addPoster("Hero 10");
+        movies.addPoster("Hero 11");
+        movies.addPoster("Hero 12");
+
+        String[] expected = {"Hero 12", "Hero 11","Hero 10","Hero 9","Hero 8","Hero 7" };
+        String[] actual = movies.giveLastPosters(6);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void giveLast10PostersDefaultTest() {
+        PosterManager movies = new PosterManager();
+        movies.addPoster("Hero 1");
+        movies.addPoster("Hero 2");
+        movies.addPoster("Hero 3");
+        movies.addPoster("Hero 4");
+        movies.addPoster("Hero 5");
+        movies.addPoster("Hero 6");
+        movies.addPoster("Hero 7");
+        movies.addPoster("Hero 8");
+
+        String[] expected = {"Hero 8","Hero 7", "Hero 6", "Hero 5", "Hero 4", "Hero 3", "Hero 2", "Hero 1" };
+        String[] actual = movies.giveLastPosters(11);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
